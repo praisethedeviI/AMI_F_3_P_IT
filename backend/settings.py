aba913fd973771911a7e2012213fe5b6ea67ba36
PrincePepper <semyon3336@gmail.com>
2021-02-02 16:48:44 +1000

    # heroku

--- backend/settings.py
+++ backend/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = "validation/templates"
-# STATICFILES_DIRS = [BASE_DIR / "validation/static", ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
